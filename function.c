b35eab63ef244c4d69b84ebd7f051fd19240df0a
Abubakar-2506022 <absiddik2506022@gmail.com>
2026-04-17 01:24:18 +0600

    Create function.c

---
+++ function.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+#include<math.h>
+void ph();
+void Gb();
+void greetings();
+int main(){
+  void ph(){
+   printf("Hello!\n");
+
+  }
+  void Gb(){
+  printf("Good Bye\n");
+  }
+  int a,b;
+  printf("please enter an integer(random). This is 1st :you will receive a greeting from me\n");
+  scanf("%d",&a);
+   ph();
+   printf(" please again enter an integer(random).This is last :you will receive a ending message from me\n");
+   scanf("%d",&b);
+   Gb();
+  void greetings(){
+    
+  
+  
+  
+  
+  
+  }
+
+}
+
+
